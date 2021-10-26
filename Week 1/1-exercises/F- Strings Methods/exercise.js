867cc077381b93188c2d5df9f5b6ed96bce137cc
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-10-26 08:51:54 +0330

    Do strings methods

---
+++ Week 1/1-exercises/F- Strings Methods/exercise.js
@@ -0,0 +1,7 @@
+// Start by creating a variable `message`
+var myName = "Amir";
+var myNameLength = myName.length;
+
+var message = `My name is ${myName} and my name is ${myNameLength} characters long`;
+
+console.log(message);
---
+++ Week 1/1-exercises/F- Strings Methods/exercise2.js
@@ -0,0 +1,8 @@
+var hisName = " Daniel  ";
+
+var trimName = hisName.trim();
+var trimNameLength = trimName.length;
+
+var message = `My name is ${trimName} and my name is ${trimNameLength} characters long`;
+
+console.log(message);
