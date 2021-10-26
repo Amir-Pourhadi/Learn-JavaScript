58f9eec2c7fb64bb0e35d2a9dce7bea303d2a57a
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-10-26 08:07:10 +0330

    Do exercise B

---
+++ Week 1/1-exercises/B- Hello World/exercise.js
@@ -0,0 +1,12 @@
+/* Try to console.log() something different.
+For example, 'Hello World. I just started learning JavaScript!'. */
+console.log("Hello World. I just started learning JavaScript!");
+
+// Try to console.log() several things at once.
+console.log("Hello World.", "I'm Amir Pourhadi!", "Full Stack Web Developer");
+
+// What happens when you get rid of the quote marks?
+// console.log(Hello world);  => Error: Hello & world are not variables!
+
+// What happens when you console.log() just a number without quotes?
+console.log(25); //=> Output is number NOT a string.
