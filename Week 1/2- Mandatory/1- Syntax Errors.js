4d8ffbe19c2415a8679847bac66d3943759a57fe
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-10-26 11:19:48 +0330

    Do syntax errors mandatory

--- Week 1/2- Mandatory/1- Syntax Errors.js
+++ Week 1/2- Mandatory/1- Syntax Errors.js
@@ -1,36 +1,34 @@
 // There are syntax errors in this code - can you fix it to pass the tests?
 
-function addNumbers(a b c) {
-    return a + b + c;
+function addNumbers(a, b, c) {
+  return a + b + c;
 }
 
-function introduceMe(name, age)
-return "Hello, my name is " + name "and I am " age + "years old";
+function introduceMe(name, age) {
+  return "Hello, my name is " + name + " and I am " + age + " years old";
+}
 
 function getTotal(a, b) {
-    total = a ++ b;
+  const total = a + b;
 
-    // Use string interpolation here
-    return "The total is %{total}"
+  // Use string interpolation here
+  return `The total is ${total}`;
 }
 
-/* ======= TESTS - DO NOT MODIFY =====
-There are some Tests in this file that will help you work out if your code is working.
+/* -------------------------- TESTS - DO NOT MODIFY ------------------------- */
+// There are some Tests in this file that will help you work out if your code is working.
 
-To run these tests type `node 1-syntax-errors.js` into your terminal
-*/
-
-const util = require('util');
+const util = require("util");
 
 function test(test_name, actual, expected) {
-    let status;
-    if (actual === expected) {
-        status = "PASSED";
-    } else {
-        status = `FAILED: expected: ${util.inspect(expected)} but your function returned: ${util.inspect(actual)}`;
-    }
+  let status;
+  if (actual === expected) {
+    status = "PASSED";
+  } else {
+    status = `FAILED: expected: ${util.inspect(expected)} but your function returned: ${util.inspect(actual)}`;
+  }
 
-    console.log(`${test_name}: ${status}`);
+  console.log(`${test_name}: ${status}`);
 }
 
 test("fixed addNumbers function - case 1", addNumbers(3, 4, 6), 13);
