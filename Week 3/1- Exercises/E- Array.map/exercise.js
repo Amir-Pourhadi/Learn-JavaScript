7bd599ac16a67891506b775df2b840149bc14e23
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-10-26 20:53:16 +0330

    Do array.map exercise

--- Week 3/1- Exercises/E- Array.map/exercise.js
+++ Week 3/1- Exercises/E- Array.map/exercise.js
@@ -1,5 +1,30 @@
 // Using the .map() method, create a new array with `numbers` multiplied by 100
 // Write multiple solutions using different syntax (as shown in the README)
 
-var numbers = [0.1, 0.2, 0.3, 0.4, 0.5];
+var nums = [0.1, 0.2, 0.3, 0.4, 0.5];
 
+function multiplyHundred(num) {
+  return num * 100;
+}
+
+var newNums1 = nums.map(multiplyHundred);
+
+var newNums2 = nums.map(function multiplyHundred(num) {
+  return num * 100;
+});
+
+var newNums3 = nums.map(function (num) {
+  return num * 100;
+});
+
+var newNums4 = nums.map((num) => {
+  return num * 100;
+});
+
+var newNums5 = nums.map((num) => num * 100);
+
+console.log(newNums1);
+console.log(newNums2);
+console.log(newNums3);
+console.log(newNums4);
+console.log(newNums5);
