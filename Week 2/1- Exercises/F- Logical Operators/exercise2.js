50fd8b15adc2751a955b0edabb057f7b7526c444
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-10-26 13:13:03 +0330

    Do logical operators exercise

--- Week 2/1- Exercises/F- Logical Operators/exercise2.js
+++ Week 2/1- Exercises/F- Logical Operators/exercise2.js
@@ -5,7 +5,21 @@
   Update the code so that you get the expected result.
 */
 
-function isNegative() {}
+function isNegative(num) {
+  return num < 0;
+}
+
+function isBetween5and10(num) {
+  return num <= 10 && num > 5;
+}
+
+function isShortName(name) {
+  return name.length < 7;
+}
+
+function startsWithD(name) {
+  return name[0] === "D";
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
@@ -24,5 +38,5 @@ console.log("Does Daniel start with 'D'?", startsWithD("Daniel"));
   Is 5 a negative number? false
   Is 10 in the range 5-10? true
   Is Daniel a short name? true
-  Does Daniel start with 'D'?
+  Does Daniel start with 'D'? true
 */
