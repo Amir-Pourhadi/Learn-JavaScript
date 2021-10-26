d4c489aad24482d9b616ec389579c3b00ab7cb82
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-10-26 21:36:52 +0330

    Do array methods 2

--- Week 3/1- Exercises/H- Array Methods 2/exercise2.js
+++ Week 3/1- Exercises/H- Array Methods 2/exercise2.js
@@ -1,21 +1,23 @@
 /*
   Array methods - .join()
   -------------------------
-  Complete the capitalise function 
+  Complete the capitalize function 
   It should return a string with the first letter in uppercase
-  For example, capitailise("hello") should return "Hello"
+  For example, capitalize("hello") should return "Hello"
   Tip: use the string method .split() and the array method .join()
 */
 
-function capitalise(str) {}
+function capitalize(str) {
+  return str.slice(0, 1).toUpperCase() + str.slice(1);
+}
 
 /* 
   DO NOT EDIT BELOW THIS LINE
   --------------------------- */
-var name = "daniel";
+var TheName = "daniel";
 
-console.log(capitalise(name));
-console.log(capitalise("hello"));
+console.log(capitalize(TheName));
+console.log(capitalize("hello"));
 
 /* 
   EXPECTED RESULT
