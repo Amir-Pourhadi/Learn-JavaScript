59a2bd3fd9f377d88e27a3162a6b7dde6ba3df2a
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-10-26 08:54:53 +0330

    Do numbers exercise

---
+++ Week 1/1-exercises/G- Numbers/exercise.js
@@ -0,0 +1,7 @@
+// Start by creating a variables `numberOfStudents` and `numberOfMentors`
+var numberOfStudents = 15;
+var numberOfMentors = 8;
+
+var total = numberOfStudents + numberOfMentors;
+
+console.log(`Total number of students and mentors: ${total}`);
