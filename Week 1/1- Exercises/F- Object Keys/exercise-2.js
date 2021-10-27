09243713aa7f38f2864e87f1de87e85533201f12
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-10-27 22:33:01 +0330

    Do object keys exercise

--- Week 1/1- Exercises/F- Object Keys/exercise-2.js
+++ Week 1/1- Exercises/F- Object Keys/exercise-2.js
@@ -1,25 +1,28 @@
 /*
 
-You have an object of mentors and their ages below, but the keys are inconsistent. JOSH and JAMIE are uppercase, while james and Mozafar are not. You want to return an array of mentors with the names in uppercase.
+You have an object of mentors and their ages below, but the keys are inconsistent.
+JOSH and JAMIE are uppercase, while james and Mozafar are not.
+You want to return an array of mentors with the names in uppercase.
 
-You're going to have to used what we learned at the start of this lesson, and some other JavaScript methods to accomplish this task.
+You're going to have to used what we learned at the start of this lesson,
+and some other JavaScript methods to accomplish this task.
 
 */
 
-let mentorsAges = {
+const mentorsAges = {
   james: 29,
   JOSH: 35,
   JAMIE: 25,
-  Mozafar: 30
+  Mozafar: 30,
 };
 
 // ONLY EDIT BELOW THIS LINE
 
-let mentorsNames = ;
+const mentorsNames = Object.keys(mentorsAges);
 
-let mentorsNamedUppercased = ;
+const mentorsNamedUppercase = mentorsNames.map((name) => name.toUpperCase());
 
 // ONLY EDIT ABOVE THIS LINE
 
-console.log(mentorsNamedUppercased);
+console.log(mentorsNamedUppercase);
 // prints [ 'JAMES', 'JOSH', 'JAMIE', 'MOZAFAR' ]
