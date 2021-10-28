707970b53661b725d1f7c5dc09e8a20a616ac16d
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-10-28 23:49:55 +0330

    Do dom practice exercise

--- Week 3/1- Exercises/E- DOM Practice/main.js
+++ Week 3/1- Exercises/E- DOM Practice/main.js
@@ -1,25 +1,11 @@
-console.log("Testing JS file loaded!")
-
 // Task 1
-
 // Without changing any of the HTML or CSS, update the <section> tags so that they have white backgrounds.
 
-
-
-
-
+const sections = document.querySelectorAll("section");
+sections.forEach((section) => (section.style.backgroundColor = "white"));
 
 // Task 2
-
 // Without changing any of the HTML or CSS, update the images on the page so that they are all centered.
-
 // Hint: look at the CSS to see if there are any classes already written which you can use.
 
-
-
-
-
-
-// Task 3
-
-// Google the date of birth and death of each of the people on the page. Without changing any of the HTML or CSS, add this in a paragraph to the end of their <section>.
+document.querySelectorAll("img").forEach((image) => image.classList.add("content-title"));
