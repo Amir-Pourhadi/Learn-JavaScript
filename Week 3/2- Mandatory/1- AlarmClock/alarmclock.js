2ff6df2335aad403d07e7f698c186c71ec37b5fd
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-10-29 12:32:32 +0330

    Do alarm clock

--- Week 3/2- Mandatory/1- AlarmClock/alarmclock.js
+++ Week 3/2- Mandatory/1- AlarmClock/alarmclock.js
@@ -1,17 +1,43 @@
-function setAlarm() {}
+function setAlarm() {
+  const counter = document.querySelector("#timeRemaining");
+  const input = document.querySelector("#alarmSet");
+  let seconds = +input.value;
+  input.value = "";
+
+  if (seconds > 59 || seconds < 1) {
+    alert("seconds should be between 1 to 59!");
+    return;
+  }
+
+  const timer = setInterval(() => {
+    if (seconds === 0) {
+      playAlarm();
+      document.body.style.backgroundColor = "rgba(255,0,0,0.7)";
+      clearInterval(timer);
+    }
+    counter.innerHTML = `00:${(seconds--).toString().padStart(2, "0")}`;
+  }, 1000);
+}
 
 // DO NOT EDIT BELOW HERE
 
 var audio = new Audio("alarmsound.mp3");
 
 function setup() {
-  document.getElementById("set").addEventListener("click", () => {
-    setAlarm();
+  const input = document.querySelector("#alarmSet");
+  const setBtn = document.getElementById("set");
+
+  setBtn.addEventListener("click", setAlarm);
+
+  input.addEventListener("input", () => {
+    input.value ? setBtn.removeAttribute("disabled") : setBtn.setAttribute("disabled", true);
   });
 
-  document.getElementById("stop").addEventListener("click", () => {
-    pauseAlarm();
+  input.addEventListener("keypress", (e) => {
+    e.keyCode === 13 && setAlarm();
   });
+
+  document.getElementById("stop").addEventListener("click", pauseAlarm);
 }
 
 function playAlarm() {
@@ -22,4 +48,4 @@ function pauseAlarm() {
   audio.pause();
 }
 
-window.onload = setup;
+window.addEventListener("load", setup);
