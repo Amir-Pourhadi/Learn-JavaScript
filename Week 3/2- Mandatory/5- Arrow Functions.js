707cf75543b879b766a42aa94017e59eee165d6c
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-10-30 01:31:05 +0330

    Do arrow functions mandatory

--- Week 3/2- Mandatory/5- Arrow Functions.js
+++ Week 3/2- Mandatory/5- Arrow Functions.js
@@ -1,19 +1,21 @@
 // Implement arrow function no variables
-
-const noVar = /*..*/ => { return "Hello there" }
-
+const noVar = () => {
+  return "Hello there";
+};
 
 // Implement arrow function with one variable
-
-const oneVar = /*..*/ => { return `You entered ${param}` }
+const oneVar = (param) => {
+  return `You entered ${param}`;
+};
 
 // Implement arrow function with two variables
-
-const twoVar = /*..*/ => { return `You have ${amount} ${fruit}` }
+const twoVar = (amount, fruit) => {
+  return `You have ${amount} ${fruit}`;
+};
 
 // This arrow function should return the object literal:
-// {fruit: "apple", colour: "red} 
+// {fruit: "apple", colour: "red}
 
-const objLiteral = /*..*/ => /*..*/
-
-// What else can be improved for the first three functions?
+const objLiteral = ({ fruit, colour }) => {
+  return `You have ${colour} ${fruit}`;
+};
