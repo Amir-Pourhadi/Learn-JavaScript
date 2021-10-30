41001bff61b44ecb5ea19ae9baa2419ec39f4b2b
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-10-30 10:20:45 +0330

    Add function scope

---
+++ Week 1/4- Further Learning/function Scope.js
@@ -0,0 +1,92 @@
+// Uncomment each section and give reasons for the output
+
+/* function collectEggs() {
+	let totalEggs = 6;
+}
+
+collectEggs();
+console.log(totalEggs); // not defined!
+ */
+
+/* -------------------------------------------------------------------------- */
+
+/* let totalEggs = 0;
+function collectEggs() {
+	totalEggs = 6;
+}
+
+console.log(totalEggs); // 0
+collectEggs();
+console.log(totalEggs); // 6
+ */
+
+/* -------------------------------------------------------------------------- */
+
+/* let totalEggs = 0;
+function collectEggs() {
+	let totalEggs = 6;
+}
+
+collectEggs();
+console.log(totalEggs); // 0
+ */
+
+/* -------------------------------------------------------------------------- */
+
+//!!! NEVER TRY IT AT HOME !!!
+/* function collectEggs() {
+	totalEggs = 6;
+}
+
+collectEggs();
+console.log(totalEggs); // 6
+ */
+
+/* -------------------------------------------------------------------------- */
+
+//! NEVER EVER DO IT
+/* for (i = 0; i < 10; i++) {
+	console.log(i); // 0 1 2 3 4 5 6 7 8 9
+}
+
+console.log(i); // 10
+ */
+
+/* -------------------------------------------------------------------------- */
+
+/* const totalEggs = 0;
+function collectEggs() {
+	const totalEggs = 6;
+	console.log(totalEggs);
+}
+
+console.log(totalEggs); // 0
+collectEggs(); // 6
+console.log(totalEggs); // 0
+ */
+
+/* -------------------------------------------------------------------------- */
+
+/* let deadlyAnimal = "Blue-Ringed Octopus";
+
+function handleAnimal() {
+	deadlyAnimal = "Scorpion fish";
+	console.log(deadlyAnimal);
+}
+
+console.log(deadlyAnimal); // Blue-Ringed Octopus
+handleAnimal(); // Scorpion fish
+ */
+
+/* -------------------------------------------------------------------------- */
+
+/* function collectEggs() {
+	var totalEggs = 6;
+
+	//FUNCTION SCOPE
+}
+
+console.log(totalEggs); // not defined!
+collectEggs();
+console.log(totalEggs); // not defined!
+ */
