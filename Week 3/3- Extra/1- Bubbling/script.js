8e800a800d6ce98c3b28c1254c722774b3e621aa
Amir Pourhadi <Alex.CE1379@Gmail.com>
2021-10-30 17:30:16 +0330

    remove unuse decleration

--- Week 3/3- Extra/1- Bubbling/script.js
+++ Week 3/3- Extra/1- Bubbling/script.js
@@ -2,10 +2,10 @@ const button = document.querySelector("#changeColor");
 const container = document.querySelector("#container");
 
 button.addEventListener("click", function (e) {
-	container.style.backgroundColor = randColor();
-	e.stopPropagation();
+  container.style.backgroundColor = randColor();
+  e.stopPropagation();
 });
 container.addEventListener("click", () => container.classList.toggle("hide"));
 
-const randColor = () => (newColor = `rgb(${randNum()}, ${randNum()}, ${randNum()})`);
+const randColor = () => `rgb(${randNum()}, ${randNum()}, ${randNum()})`;
 const randNum = () => Math.floor(Math.random() * 256);
